58b3a54595a5328d199858b0b7da8f0ea41f765b
Andrew Wheeler <andrew@smallbytes.app>
2020-01-30 14:29:38 -0600

    Changed styling

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  url: "https://lumen.netlify.com",
+  url: "https://www.writingforwheel.com",
   pathPrefix: "/",
   title: "Blog by Andrew Wheeler",
   subtitle:
@@ -17,7 +17,7 @@ module.exports = {
       path: "/"
     },
     {
-      label: "About me",
+      label: "About",
       path: "/pages/about"
     },
     {
